d993c43453c93bdc0ac616c492c80a5ffd0f9b91
mac8695 <Mahesh.Chavan@MyGeorgian.ca>
2017-08-07 17:49:16 -0400

    Add project files.

---
+++ LiquorStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LiquorStore.MvcApplication" Language="C#" %>
---
+++ LiquorStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LiquorStore.Startup))]
+namespace LiquorStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
